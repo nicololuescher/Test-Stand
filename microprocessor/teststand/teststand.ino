92b93450cebd1bce241ab35c0cc475283da749ed
Nicolo Lüscher <nicolo.luescher@students.bfh.ch>
2022-06-01 19:45:33 +0200

    improved password security

--- microprocessor/teststand/teststand.ino
+++ microprocessor/teststand/teststand.ino
@@ -3,7 +3,7 @@
  * Author: Nicolo Lüscher
  * 2022
  */
-  
+
 #include <Arduino.h>
 #include "HX711.h"
 #include "soc/rtc.h"
@@ -11,14 +11,14 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 
-const char *SSID = WIFI_SSID;
-const char *PWD = WIFI_PASSWORD;
+const char *SSID = YOUR_SSID;
+const char *PWD = YOUR_PASSWORD;
 
-String mqttUser = MQTT_USER;
-String mqttPassword = MQTT_PASSWORD;
+const char *mqttUser = YOUR_MQTT_USER;
+const char *mqttPassword = YOUR_MQTT_PASSWORD;
 
-char *mqttServer = MQTT_SERVER;
-int mqttPort = MQTT_PORT;
+char *mqttServer = YOUR_MQTT_SERVER;
+int mqttPort = YOUR_MQTT_PORT;
 
 // HX711 circuit wiring
 const int LOADCELL_DOUT_PIN = 16;
@@ -30,52 +30,60 @@ HX711 scale;
 
 WiFiClient wifiClient;
 
-void callback(char* topic, byte* payload, unsigned int length) {
+void callback(char *topic, byte *payload, unsigned int length)
+{
   // handle message arrived
 }
 
-PubSubClient client(mqttServer, mqttPort, callback, wifiClient); 
+PubSubClient client(mqttServer, mqttPort, callback, wifiClient);
 
-void setupMQTT() {
-  Serial.print("Connecting to MQTT Server");
-    if (client.connect("arduinoClient", mqttUser, mqttPassword)) {
-    Serial.print("Connected!");
+void setupMQTT()
+{
+  Serial.println("Connecting to MQTT Server");
+  if (client.connect("arduinoClient", mqttUser, mqttPassword))
+  {
+    Serial.println("Connected!");
     client.publish("Status", "ESP Connected!");
-  } else {
-    Serial.print("Connection Error!");
+  }
+  else
+  {
+    Serial.println("Connection Error!");
   }
 }
 
-void connectToWiFi() {
+void connectToWiFi()
+{
   Serial.print("Connecting to ");
- 
+
   WiFi.begin(SSID, PWD);
   Serial.println(SSID);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     Serial.print(".");
     delay(500);
   }
-  Serial.print("Connected.");
-  
+  Serial.println("Connected.");
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   rtc_clk_cpu_freq_set(RTC_CPU_FREQ_80M);
 
   Serial.println("Initializing scale");
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-     
+
   scale.set_scale(91.3);
-  scale.tare();               // reset the scale to 0
-  
+  scale.tare(); // reset the scale to 0
+
   Serial.println("Initialization Complete!");
   connectToWiFi();
   setupMQTT();
 }
 
-void loop() {
-    dtostrf(scale.get_units(), 6, 2, buff);
-    String output = "{\"time\": " + String(millis()) + ", \"data\": " + buff + "}";
-    client.publish("reading", output.c_str());
+void loop()
+{
+  dtostrf(scale.get_units(), 6, 2, buff);
+  String output = "{\"time\": " + String(millis()) + ", \"data\": " + buff + "}";
+  client.publish("reading", output.c_str());
 }
